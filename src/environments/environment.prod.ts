f940edda4a874ac7b1fa1f9b8e24b11bd59da560
Javier Cintron <javier.cintron@oasisdigital.com>
2019-12-06 22:02:04 -0400

    remove environments

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  githubToken: 'da8b3ee9c0ae174a2bf094a16842f06051f7d161',
-  graphApiUri: 'https://api.github.com',
-  restApiUri: ' https://api.github.com',
-  useGraphQl: true
-};
